c04af1a9eb87699a463d05db660d643f0f65e818
Norbert <norbertguzy@o2.pl>
2023-03-29 18:56:12 +0200

    Incrementing numbers animation

---
+++ IncrementingNumbers/scripts.js
@@ -0,0 +1,19 @@
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.textContent = "0";
+
+  const updateCounter = () => {
+    const target = Number(counter.getAttribute("data-target"));
+    const c = Number(counter.textContent);
+
+    const increment = target / 300;
+
+    if (c < target) {
+      counter.textContent = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    }
+  };
+
+  updateCounter();
+});
